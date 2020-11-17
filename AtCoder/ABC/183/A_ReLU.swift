ac4c048ce182921f2cc77c363a907c24cb6996f4
uhooi <uhooi@server.fake>
2020-11-17 17:55:43 +0900

    Refactor ABC 183 A

--- AtCoder/ABC/183/A_ReLU.swift
+++ AtCoder/ABC/183/A_ReLU.swift
@@ -1,12 +1,10 @@
 // https://atcoder.jp/contests/abc183/tasks/abc183_a
 
 let x = Int(readLine()!)!
+precondition(-10 <= x && x <= 10)
+
 print(reLU(x))
 
 private func reLU(_ x: Int) -> Int {
-    if x < 0 {
-        return 0
-    } else {
-        return x
-    }
+    max(0, x)
 }
