97a1f558fcca0ed767e6835a3f7c95e57e943963
uhooi <uhooi@server.fake>
2020-11-17 13:05:25 +0900

    Fix Practice A

--- AtCoder/Practice/A_WelcomeToAtCoder.swift
+++ AtCoder/Practice/A_WelcomeToAtCoder.swift
@@ -1,11 +1,15 @@
 // https://atcoder.jp/contests/abs/tasks/practice_1
 
 let a = Int(readLine()!)!
+precondition(1 <= a && a <= 1000)
 
-let line2 = readLine()!.split(separator: " ").map { Int($0)! }
-let b = line2[0]
-let c = line2[1]
+let bc = readLine()!.split(separator: " ").map { Int($0)! }
+let b = bc[0]
+precondition(1 <= b && b <= 1000)
+let c = bc[1]
+precondition(1 <= c && c <= 1000)
 
 let s = readLine()!
+precondition(1 <= s.count && s.count <= 100)
 
 print(a + b + c, s)
