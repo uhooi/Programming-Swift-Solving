887c9ba98c87cd2eee78d52a7436d48a2e107718
uhooi <uhooi@server.fake>
2020-11-18 19:33:52 +0900

    Add CODE FESTIVAL 2017 qual C C

---
+++ AtCoder/CodeFestival/2017/QualC/C_InsertingX.swift
@@ -0,0 +1,7 @@
+// https://atcoder.jp/contests/code-festival-2017-qualc/tasks/code_festival_2017_qualc_c
+
+import Foundation
+
+// FIXME: WA
+let s = readLine()!
+precondition(1 <= s.count && Double(s.count) <= pow(10, 5))
