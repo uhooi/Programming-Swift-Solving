c248131c8aa42a5a438166e98d1922e78733b24d
uhooi <uhooi@server.fake>
2020-11-19 19:40:33 +0900

    Fix ABC 081 B

--- AtCoder/ABC/081/B_ShiftOnly.swift
+++ AtCoder/ABC/081/B_ShiftOnly.swift
@@ -2,23 +2,19 @@
 
 let n = Int(readLine()!)!
 precondition(1 <= n && n <= 200)
-let aa = readLine()!.split(separator: " ").map { Int($0)! }
+var aa = readLine()!.split(separator: " ").map { Int($0)! }
 precondition(aa.count == n)
 
 var answer = 0
-for a in aa {
-    if a % 2 != 0 {
-        print(answer)
-        return
+var isAllEven = true
+while isAllEven {
+    for a in aa where a % 2 != 0 {
+        isAllEven = false
+        break
+    }
+    if isAllEven {
+        aa = aa.map { $0 / 2 }
+        answer += 1
     }
 }
-var aa = aa.map { $0 / 2 }
-for a in aa {
-    if a % 2 != 0 {
-        print(answer)
-        return
-    }
-}
-
-
-private func isAll
+print(answer)
