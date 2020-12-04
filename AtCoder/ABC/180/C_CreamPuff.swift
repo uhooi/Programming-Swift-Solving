bbdf467082feea6fdd93eb252ebdde1ca30aef04
uhooi <uhooi@server.fake>
2020-12-04 22:12:17 +0900

    Fix ABC 180 C

--- AtCoder/ABC/180/C_CreamPuff.swift
+++ AtCoder/ABC/180/C_CreamPuff.swift
@@ -6,16 +6,11 @@ let n = Int(readLine()!)!
 precondition(1 <= n && Double(n) <= pow(10, 12))
 
 var secondHalfs: [Int] = []
-for i in 1...n {
+for i in 1...(Int(floor(sqrt(Double(n))))) where n % i == 0 {
+    print(i)
     let secondHalf = n / i
-    if secondHalf < i {
-        break
-    }
-    if n % i == 0 {
-        print(i)
-        if secondHalf != i {
-            secondHalfs.insert(secondHalf, at: 0)
-        }
+    if secondHalf != i {
+        secondHalfs.insert(secondHalf, at: 0)
     }
 }
 
