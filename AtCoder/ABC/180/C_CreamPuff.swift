41cac6007fcc77e2c2439a5ef15b2dedb3f13f8c
uhooi <uhooi@server.fake>
2020-12-04 20:50:31 +0900

    Fix ABC 180 C

--- AtCoder/ABC/180/C_CreamPuff.swift
+++ AtCoder/ABC/180/C_CreamPuff.swift
@@ -6,8 +6,10 @@ import Foundation
 let n = Int(readLine()!)!
 precondition(1 <= n && Double(n) <= pow(10, 12))
 
-for i in 1...n {
+let secondLargest = n % 2 == 0 ? n / 2 : (n - 1) / 2
+for i in 1...secondLargest {
     if n % i == 0 {
         print(i)
     }
 }
+print(n)
