69bba4d029344b6bdfaec86ca40b236a6d877dc7
uhooi <uhooi@server.fake>
2020-12-05 23:03:03 +0900

    Add ARC 110 A

--- AtCoder/ARC/110/A_RedundantRedundancy.swift
+++ AtCoder/ARC/110/A_RedundantRedundancy.swift
@@ -5,5 +5,41 @@ import Foundation
 let n = Int(readLine()!)!
 precondition(2 <= n && n <= 30)
 
-// FIXME: WA
-print((2...n).reduce(1, *) + 1)
+var nn: [Int] = []
+for i in 2...n {
+    let iPrimeFactorizations = primeFactorizations(of: i)
+    for j in Set(iPrimeFactorizations) {
+        let loopCount = ((iPrimeFactorizations.filter { $0 == j } .count) - (nn.filter { $0 == j } .count))
+        if loopCount <= 0 {
+            break
+        }
+        for _ in 1...loopCount {
+            nn.append(j)
+        }
+    }
+}
+
+print(nn.reduce(1, *) + 1)
+
+private func primeFactorizations(of value: Int) -> [Int] {
+    precondition(value > 0)
+    if (1...3).contains(value) {
+        return [value]
+    }
+
+    var copiedValue = value
+    var primeFactorizations: [Int] = []
+    for i in 2...Int(floor(Double(n) / 2)) {
+        while copiedValue % i == 0 {
+            copiedValue /= i
+            primeFactorizations.append(i)
+        }
+        if primeFactorizations.reduce(1, *) == value {
+            break
+        }
+    }
+    if primeFactorizations.isEmpty {
+        primeFactorizations.append(value)
+    }
+    return primeFactorizations
+}
