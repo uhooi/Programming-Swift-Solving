9beb378b7af0adef58248ceb9c185d7f6866e468
uhooi <uhooi@server.fake>
2020-12-05 23:01:50 +0900

    Add ARC 110 A

--- AtCoder/ARC/110/A_RedundantRedundancy.swift
+++ AtCoder/ARC/110/A_RedundantRedundancy.swift
@@ -3,20 +3,5 @@
 let n = Int(readLine()!)!
 precondition(2 <= n && n <= 30)
 
-// FIXME: TLE
-var answer: Int?
-for i in n...10000000000000 {
-    for j in 2...n {
-        if i % j != 1 {
-            break
-        }
-        if j == n {
-            answer = i
-            break
-        }
-    }
-    if answer != nil {
-        break
-    }
-}
-print(answer!)
+// FIXME: WA
+print((2...n).reduce(1, *) + 1)
