28a73f5f6cab4c94aa495fed0cb620e40a27a58f
uhooi <uhooi@server.fake>
2020-12-13 23:49:44 +0900

    Fix ABC 185 C

--- AtCoder/ABC/185/C_DuodecimFerra.swift
+++ AtCoder/ABC/185/C_DuodecimFerra.swift
@@ -1,19 +1,14 @@
 // https://atcoder.jp/contests/abc185/tasks/abc185_c
 
-// FIXME: WA
 import Foundation
- 
+
 let l = Int(readLine()!)!
 precondition(12 <= l && l <= 200)
- 
-let answer: Int
-switch l {
-case 12:
-    answer = 1
-case 13...:
-    answer = ((l - 11)...(l - 1)).reduce(1, *) / (2...11).reduce(1, *)
-default:
-    preconditionFailure("`l` is an invalid value.")
+
+var answer = 1
+for i in 1...11 {
+    answer *= l - i
+    answer /= i
 }
 precondition(Double(answer) < pow(2, 63))
 print(answer)
