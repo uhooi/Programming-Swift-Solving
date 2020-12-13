6086602e6802b53e08e58ac2f4027b426f83ccbd
uhooi <uhooi@server.fake>
2020-12-13 23:06:04 +0900

    Add ABC 185 B

---
+++ AtCoder/ABC/185/B_SmartphoneAddiction.swift
@@ -0,0 +1,34 @@
+import Foundation
+ 
+let nmt = readLine()!.split(separator: " ").map { Int($0)! }
+let (n, m, t) = (nmt[0], nmt[1], nmt[2])
+precondition(1 <= n && Double(n) <= pow(10, 9))
+precondition(1 <= m && m <= 1_000)
+precondition(1 <= t && Double(t) <= pow(10, 9))
+ 
+var aabb: [[Int]] = []
+for _ in 1...m {
+    let ab = readLine()!.split(separator: " ").map { Int($0)! }
+    aabb += [ab]
+}
+precondition(aabb.count == m)
+ 
+var battery = n
+var caffeeCount = 0
+var result = "Yes"
+for time in 0..<t {
+    let halfTime = Double(time) + 0.5
+    if Double(aabb[caffeeCount][0]) <= halfTime && halfTime <= Double(aabb[caffeeCount][1]) {
+        battery = min(battery + 1, n)
+        if halfTime + 0.5 == Double(aabb[caffeeCount][1]) {
+            caffeeCount = min(caffeeCount + 1, m - 1)
+        }
+    } else {
+        battery = max(battery - 1, 0)
+        if battery == 0 {
+            result = "No"
+            break
+        }
+    }
+}
+print(result)
