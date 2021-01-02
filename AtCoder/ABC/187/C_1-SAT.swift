b3dd12cd495b64f33050e30e284c6766a11c9129
uhooi <uhooi@server.fake>
2021-01-02 23:21:14 +0900

    Fix ABC 187 C

--- AtCoder/ABC/187/C_1-SAT.swift
+++ AtCoder/ABC/187/C_1-SAT.swift
@@ -6,25 +6,23 @@ import Foundation
 let n = Int(readLine()!)!
 precondition(1 <= n && Double(n) <= Double(2) * pow(10, 5))
 
-var ssSet: Set<String> = []
+var ssNotExclamationSet: Set<String> = []
+var ssExclamationSet: Set<String> = []
 for _ in 1...n {
-    ssSet.insert(readLine()!)
+    let s = readLine()!
+    if s.first == "!" {
+        ssExclamationSet.insert(String(s.dropFirst(1)))
+    } else {
+        ssNotExclamationSet.insert(s)
+    }
 }
-let ss = Array(ssSet)
-precondition(ss.count == n)
-precondition(ss.allSatisfy { 1 <= $0.count && $0.count <= 10 })
+precondition(ssNotExclamationSet.allSatisfy { 1 <= $0.count && $0.count <= 10 })
+precondition(ssExclamationSet.allSatisfy { 1 <= $0.count && $0.count <= 10 })
 
 var answer = "satisfiable"
-for i in 0..<(ss.count - 1) {
-    for j in (i + 1)..<ss.count {
-        if ss[i] == "!" + ss[j] {
-            answer = ss[j]
-            break
-        }
-        if "!" + ss[i] == ss[j] {
-            answer = ss[i]
-            break
-        }
+for i in ssNotExclamationSet {
+    for j in ssExclamationSet where i == j {
+        answer = i
     }
     if answer != "satisfiable" {
         break
