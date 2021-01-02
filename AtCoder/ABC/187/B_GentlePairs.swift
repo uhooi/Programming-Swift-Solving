a5ccb2c44f41c458f2eeedfe184d07c466069a12
uhooi <uhooi@server.fake>
2021-01-02 23:09:04 +0900

    Add ABC 187 B

---
+++ AtCoder/ABC/187/B_GentlePairs.swift
@@ -0,0 +1,31 @@
+// https://atcoder.jp/contests/abc187/tasks/abc187_b
+
+// FIXME: WA
+import Foundation
+
+let n = Int(readLine()!)!
+precondition(1 <= n && Double(n) <= pow(10, 3))
+
+var xxyy: [[Int]] = []
+for _ in 1...n {
+    xxyy.append(readLine()!.split(separator: " ").map { Int($0)! })
+}
+precondition(xxyy.count == n)
+precondition(xxyy.allSatisfy { Double(abs($0[0])) <= pow(10, 3) })
+precondition(xxyy.allSatisfy { Double(abs($0[1])) <= pow(10, 3) })
+
+var answer = 0
+for i in xxyy.indices {
+    if i >= (xxyy.count - 1) {
+        break
+    }
+    for j in (i + 1)..<xxyy.count {
+        let dy = xxyy[j][1] - xxyy[i][1]
+        let dx = xxyy[j][0] - xxyy[i][0]
+        let m = dy / dx
+        if -1 <= m && m <= 1 {
+            answer += 1
+        }
+    }
+}
+print(answer)
