048427383c057b7e676a97d21eaaa8dcb5ee79eb
uhooi <uhooi@server.fake>
2021-01-02 23:20:22 +0900

    Add ABC 187 C

---
+++ AtCoder/ABC/187/C_1-SAT.swift
@@ -0,0 +1,32 @@
+// https://atcoder.jp/contests/abc187/tasks/abc187_c
+
+// FIXME: TLE
+import Foundation
+
+let n = Int(readLine()!)!
+precondition(1 <= n && Double(n) <= Double(2) * pow(10, 5))
+
+var ss: [String] = []
+for _ in 1...n {
+    ss.append(readLine()!)
+}
+precondition(ss.count == n)
+precondition(ss.allSatisfy { 1 <= $0.count && $0.count <= 10 })
+
+var answer = "satisfiable"
+for i in 0..<(ss.count - 1) {
+    for j in (i + 1)..<ss.count {
+        if ss[i] == "!" + ss[j] {
+            answer = ss[j]
+            break
+        }
+        if "!" + ss[i] == ss[j] {
+            answer = ss[i]
+            break
+        }
+    }
+    if answer != "satisfiable" {
+        break
+    }
+}
+print(answer)
