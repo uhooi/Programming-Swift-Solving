82589b1da36bf4e9795c8f346288a3bfb99471ba
uhooi <uhooi@server.fake>
2021-01-09 19:21:14 +0900

    Fix ABC 049 C

--- AtCoder/ABC/049/C_Daydream.swift
+++ AtCoder/ABC/049/C_Daydream.swift
@@ -1,16 +1,15 @@
 // https://atcoder.jp/contests/abs/tasks/arc065_a
 
-// FIXME: TLE
 import Foundation
 
 var s = readLine()!
 precondition(1 <= s.count && Double(s.count) <= pow(10, 5))
 
 let words = ["dream", "dreamer", "erase", "eraser"]
-while s.count > 0 {
+while !s.isEmpty {
     var hasSuffix = false
     for word in words where s.hasSuffix(word) {
-        s = String(s.dropLast(word.count))
+        s.removeLast(word.count)
         hasSuffix = true
         break
     }
@@ -18,4 +17,4 @@ while s.count > 0 {
         break
     }
 }
-print(s.count == 0 ? "YES" : "NO")
+print(s.isEmpty ? "YES" : "NO")
