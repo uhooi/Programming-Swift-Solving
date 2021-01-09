7eeba90e84756e3d7d3cc04b0e68e1c67602b7b6
uhooi <uhooi@server.fake>
2021-01-09 19:12:30 +0900

    Fix ABC 049 C

--- AtCoder/ABC/049/C_Daydream.swift
+++ AtCoder/ABC/049/C_Daydream.swift
@@ -1,33 +1,21 @@
 // https://atcoder.jp/contests/abs/tasks/arc065_a
 
-import Foundation
-
 // FIXME: TLE
 import Foundation
 
 var s = readLine()!
 precondition(1 <= s.count && Double(s.count) <= pow(10, 5))
 
-var isLooping = true
-while isLooping && s.count > 0 {
-    switch s.prefix(5) {
-    case "dream":
-        s = String(s.dropFirst(5))
-        if s.prefix(5) == "erase" {
-            s = String(s.dropFirst(5))
-            if s.prefix(1) == "r" {
-                s = String(s.dropFirst(1))
-            }
-        } else if s.prefix(2) == "er" {
-            s = String(s.dropFirst(2))
-        }
-    case "erase":
-        s = String(s.dropFirst(5))
-        if s.prefix(1) == "r" {
-            s = String(s.dropFirst(1))
-        }
-    default:
-        isLooping = false
+let words = ["dream", "dreamer", "erase", "eraser"]
+while s.count > 0 {
+    var hasSuffix = false
+    for word in words where s.hasSuffix(word) {
+        s = String(s.dropLast(word.count))
+        hasSuffix = true
+        break
+    }
+    if !hasSuffix {
+        break
     }
 }
 print(s.count == 0 ? "YES" : "NO")
