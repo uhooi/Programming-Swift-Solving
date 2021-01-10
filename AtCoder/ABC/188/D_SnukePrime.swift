95343b85ce719e5e2a4d43e946a273792a6919ad
uhooi <uhooi@server.fake>
2021-01-10 22:55:27 +0900

    Fix ABC 188 C

--- AtCoder/ABC/188/D_SnukePrime.swift
+++ AtCoder/ABC/188/D_SnukePrime.swift
@@ -16,20 +16,19 @@ let aabbcc: [ABC] = (1...N).map { _ in
 precondition(aabbcc.allSatisfy { 1 <= $0.a && $0.a <= $0.b && Double($0.b) <= pow(10, 9) })
 precondition(aabbcc.allSatisfy { 1 <= $0.c && Double($0.c) <= pow(10, 9) })
 
-let tenHatNine = NSDecimalNumber(decimal: pow(10, 9)).intValue
 var sum: [Int: Int] = [:]
 for i in 1...N {
     for j in (aabbcc[i - 1].a)...(aabbcc[i - 1].b) {
+        let cost: Int
         if let value = sum[j] {
-            sum[j] = value + aabbcc[i - 1].c
+            cost = value + aabbcc[i - 1].c
         } else {
-            sum[j] = aabbcc[i - 1].c
+            cost = aabbcc[i - 1].c
         }
+        sum[j] = cost > C ? C : cost
     }
 }
 
-let sum2 = sum
-    .mapValues { $0 > C ? C : $0 }
 var answer = 0
-sum2.forEach { answer += $0.value }
+sum.forEach { answer += $0.value }
 print(answer)
