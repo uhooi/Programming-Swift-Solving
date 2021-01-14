2e4f7f0401234ac6a4921af99711f6357530977f
uhooi <uhooi@server.fake>
2021-01-14 21:16:41 +0900

    Fix sumitrust 2019 D

--- AtCoder/sumitrust/2019/D_LuckyPIN.swift
+++ AtCoder/sumitrust/2019/D_LuckyPIN.swift
@@ -1,6 +1,5 @@
 // https://atcoder.jp/contests/sumitrust2019/tasks/sumitb2019_d
 
-// FIXME: TLE
 import Foundation
 
 let n = Int(readLine()!)!
@@ -18,9 +17,15 @@ precondition(
 
 let luckyNumbers = s.map { String($0) }
 var answers: Set<String> = []
-for i in 0...(n - 3) {
-    for j in (i + 1)...(n - 2) {
-        for k in (j + 1)...(n - 1) {
+var checked1s: Set<String> = []
+for i in 0...(n - 3) where !checked1s.contains(luckyNumbers[i]) {
+    checked1s.insert(luckyNumbers[i])
+    var checked2s: Set<String> = []
+    for j in (i + 1)...(n - 2) where !checked2s.contains(luckyNumbers[j]) {
+        checked2s.insert(luckyNumbers[j])
+        var checked3s: Set<String> = []
+        for k in (j + 1)...(n - 1) where !checked3s.contains(luckyNumbers[k]) {
+            checked3s.insert(luckyNumbers[k])
             answers.insert(luckyNumbers[i] + luckyNumbers[j] + luckyNumbers[k])
         }
     }
