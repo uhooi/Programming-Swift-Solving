617b449e5107cd1d10b6c5f81d829f7e921609b2
uhooi <uhooi@server.fake>
2021-01-23 23:52:12 +0900

    Fix ABC 189 D

--- AtCoder/ABC/189/D_LogicalExpression.swift
+++ AtCoder/ABC/189/D_LogicalExpression.swift
@@ -1,6 +1,5 @@
 // https://atcoder.jp/contests/abc189/tasks/abc189_d
 
-// FIXME: TLE
 import Foundation
 
 let N = Int(readLine()!)!
@@ -10,38 +9,17 @@ let ss = (1...N).map { _ in readLine()! }
 precondition(ss.count == N)
 precondition(ss.allSatisfy { ["AND", "OR"].contains($0) })
 
-var answer = 0
-for i in 1...((NSDecimalNumber(decimal: pow(2, (N + 1))).intValue) - 1) {
-    var y: Bool? = nil
-    let xx = i.binaryString(digits: (N + 1)).map({ String($0) }).reversed().map { $0 }
-    for j in 1...xx.count {
-        let x = Bool(truncating: Int(xx[j - 1])! as NSNumber)
-        if y == nil {
-            y = x
-        } else {
-            let s = ss[j - 2]
-            if s == "AND" {
-                y = y! && x
-            } else if s == "OR" {
-                y = y! || x
-            } else {
-                preconditionFailure()
-            }
-        }
+var answer = 1
+var i = N + 1
+for s in ss.reversed() {
+    switch s {
+    case "OR":
+        answer += NSDecimalNumber(decimal: pow(2, (i - 1))).intValue
+    case "AND":
+        break
+    default:
+        preconditionFailure()
     }
-    answer += y! ? 1 : 0
+    i -= 1
 }
 print(answer)
-
-private extension FixedWidthInteger {
-    func binaryString(digits: Int) -> String {
-        var result: [String] = []
-        for i in 0..<(Self.bitWidth / 8) {
-            let byte = UInt8(truncatingIfNeeded: self >> (i * 8))
-            let byteString = String(byte, radix: 2)
-            let padding = String(repeating: "0", count: 8 - byteString.count)
-            result.append(padding + byteString)
-        }
-        return String(result.reversed().joined().suffix(digits))
-    }
-}
