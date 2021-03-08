ab783f2cc998fa91fa6574af618fe5972d5853ca
uhooi <uhooi@server.fake>
2021-03-09 00:11:20 +0900

    Fix ABC 194 C

--- AtCoder/ABC/194/C_SquaredError.swift
+++ AtCoder/ABC/194/C_SquaredError.swift
@@ -1,6 +1,5 @@
 // https://atcoder.jp/contests/abc194/tasks/abc194_c
 
-// FIXME: TLE
 import Foundation
 
 let N = Int(readLine()!)!
@@ -11,9 +10,9 @@ precondition(AA.count == N)
 precondition(AA.allSatisfy { abs($0) <= 200 })
 
 var answer = 0
-for i in 0...(N - 2) {
-    for j in i...(N - 1) {
-        answer += (AA[i] - AA[j]) * (AA[i] - AA[j])
-    }
+var aaSum = AA.reduce(0, +)
+for a in AA {
+    aaSum -= a
+    answer += (N - 1) * a * a - 2 * a * aaSum
 }
 print(answer)
