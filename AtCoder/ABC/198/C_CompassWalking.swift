2a66cdfb385a7b85c349dfecc2716e520b6f8e9d
uhooi <uhooi@server.fake>
2021-04-11 22:33:24 +0900

    Fix ABC 198 C

--- AtCoder/ABC/198/C_CompassWalking.swift
+++ AtCoder/ABC/198/C_CompassWalking.swift
@@ -1,6 +1,5 @@
 // https://atcoder.jp/contests/abc198/tasks/abc198_c
 
-// FIXME: WA
 import Foundation
 
 let rxy = readLine()!.split(separator: " ").map { Int($0)! }
@@ -10,4 +9,9 @@ precondition(0 <= X && Double(X) <= pow(10, 5))
 precondition(0 <= Y && Double(Y) <= pow(10, 5))
 precondition((X, Y) != (0, 0))
 
-print(Int(ceil(sqrt(pow(Double(X), 2) + pow(Double(Y), 2)) / Double(R))))
+let goalDistance = sqrt(pow(Double(X), 2) + pow(Double(Y), 2))
+if Double(R) > goalDistance {
+    print(2)
+} else {
+    print(Int(ceil(goalDistance / Double(R))))
+}
