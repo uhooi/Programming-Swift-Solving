ad576de85c17b2279cd4791e07b1e087b8585d74
uhooi <uhooi@server.fake>
2021-04-13 20:52:25 +0900

    Fix ABC 198 D

--- AtCoder/ABC/198/D_SendMoreMoney.swift
+++ AtCoder/ABC/198/D_SendMoreMoney.swift
@@ -7,32 +7,24 @@ precondition(1 <= S1.count && S1.count <= 10)
 precondition(1 <= S2.count && S2.count <= 10)
 precondition(1 <= S3.count && S3.count <= 10)
 
-var answer: [Int] = []
-let existingAlphabets = Set((S1 + S2 + S3).map { String($0) })
+let existingAlphabets = Array(Set((S1 + S2 + S3).map { String($0) }))
 if existingAlphabets.count > 10 {
     print("UNSOLVABLE")
 } else {
-    for numbers in permutations(of: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) {
-        for number in numbers {
-            var dictionary: [String: Int] = [:]
-            for alphabet in existingAlphabets {
-                dictionary[alphabet] = number
-            }
-            let s1String = S1.map { String(dictionary[String($0)]!) } .joined()
-            let s2String = S2.map { String(dictionary[String($0)]!) } .joined()
-            let s3String = S3.map { String(dictionary[String($0)]!) } .joined()
-            if s1String.prefix(1) == "0" || s2String.prefix(1) == "0" || s3String.prefix(1) == "0" {
-                break
-            }
-            let s1 = Int(s1String)!
-            let s2 = Int(s2String)!
-            let s3 = Int(s3String)!
-            if s1 + s2 == s3 {
-                answer = [s1, s2, s3]
-                break
-            }
+    var answer: [Int] = []
+    for numbers in permutations(of: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) {
+        let dictionary = Dictionary(uniqueKeysWithValues: zip(existingAlphabets, numbers))
+        let s1String = S1.map { String(dictionary[String($0)]!) } .joined()
+        let s2String = S2.map { String(dictionary[String($0)]!) } .joined()
+        let s3String = S3.map { String(dictionary[String($0)]!) } .joined()
+        if s1String.prefix(1) == "0" || s2String.prefix(1) == "0" || s3String.prefix(1) == "0" {
+            continue
         }
-        if !answer.isEmpty {
+        let s1 = Int(s1String)!
+        let s2 = Int(s2String)!
+        let s3 = Int(s3String)!
+        if s1 + s2 == s3 {
+            answer = [s1, s2, s3]
             break
         }
     }
