fda2d385453516a9bfb674894b82e2f9c9701682
uhooi <uhooi@server.fake>
2021-04-24 16:59:20 +0900

    Fix ABC 196 C

--- AtCoder/ABC/196/C_Doubled.swift
+++ AtCoder/ABC/196/C_Doubled.swift
@@ -1,22 +1,16 @@
 // https://atcoder.jp/contests/abc196/tasks/abc196_c
 
-// FIXME: TLE
 import Foundation
 
 let N = Int(readLine()!)!
 precondition(1 <= N && Double(N) <= pow(10, 12))
 
-var answer = 0
-for i in 1...N {
-    let iString = String(i)
-    guard iString.count % 2 == 0 else {
-        continue
-    }
-    
-    let firstHalf = iString.prefix(iString.count / 2)
-    let lastHalf = iString.suffix(iString.count / 2)
-    if firstHalf == lastHalf {
-        answer += 1
-    }
+let n: String
+if String(N).count % 2 == 0 {
+    n = String(N)
+} else {
+    n = String(N).indices.map { _ in String(9) } .joined()
 }
-print(answer)
+let firstHalf = Int(n.prefix(n.count / 2))!
+let lastHalf = Int(n.suffix(n.count / 2))!
+print(firstHalf <= lastHalf ? firstHalf : firstHalf - 1)
