c3ccaca73399b5510cd570f17fc5e95512397cfd
uhooi <uhooi@server.fake>
2021-04-24 15:35:53 +0900

    Refactor ABC 196 B

--- AtCoder/ABC/196/B_RoundDown.swift
+++ AtCoder/ABC/196/B_RoundDown.swift
@@ -1,15 +1,6 @@
 // https://atcoder.jp/contests/abc196/tasks/abc196_b
 
-import Foundation
-
 let X = readLine()!
 precondition(1 <= X.count && X.count <= 101)
 
-let pointRange = X.range(of: ".")
-let answer: String
-if let pointRange = pointRange {
-    answer = String(String(X[...pointRange.lowerBound]).dropLast())
-} else {
-    answer = X
-}
-print(answer)
+print(X.prefix(while: { $0 != "." }))
