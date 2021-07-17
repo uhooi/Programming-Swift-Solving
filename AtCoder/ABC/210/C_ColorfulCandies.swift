deb461a1e4ecd05d4a221b65412616984c7f19f1
uhooi <uhooi@server.fake>
2021-07-17 23:35:31 +0900

    Fix ABC 210 C

--- AtCoder/ABC/210/C_ColorfulCandies.swift
+++ AtCoder/ABC/210/C_ColorfulCandies.swift
@@ -1,6 +1,5 @@
 // https://atcoder.jp/contests/abc210/tasks/abc210_c
 
-// FIXME: TLE
 import Foundation
 
 let NK = readLine()!.split(separator: " ").map { Int($0)! }
@@ -10,14 +9,21 @@ precondition(1 <= K && K <= N && Double(N) <= 3 * pow(10, 5))
 let cc = readLine()!.split(separator: " ").map { Int($0)! }
 precondition(cc.allSatisfy { 1 <= $0 && Double($0) <= pow(10, 9) })
 
-var stack = cc[0...(K - 1)]
-var answer = Set(stack).count
-if N != K {
-    for i in K...(N - 1) {
-        _ = stack.popFirst()
-        stack.append(cc[i])
-        let count = Set(stack).count
-        answer = max(answer, count)
+var countDic: [Int: Int] = [:]
+for i in 0..<K {
+    countDic[cc[i], default: 0] += 1
+}
+
+var answer = countDic.count
+
+for i in K..<N {
+    let oldCandy = cc[i - K]
+    let newCandy = cc[i]
+    countDic[oldCandy, default: 0] -= 1
+    countDic[newCandy, default: 0] += 1
+    if countDic[oldCandy] == 0 {
+        countDic.removeValue(forKey: oldCandy)
     }
+    answer = max(answer, countDic.count)
 }
 print(answer)
