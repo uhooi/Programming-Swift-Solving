33108adc9813e9d679473478eb0ebe3b653e862e
uhooi <uhooi@server.fake>
2021-07-17 23:04:10 +0900

    Fix ABC 210 C

--- AtCoder/ABC/210/C_ColorfulCandies.swift
+++ AtCoder/ABC/210/C_ColorfulCandies.swift
@@ -10,11 +10,11 @@ precondition(1 <= K && K <= N && Double(N) <= 3 * pow(10, 5))
 let cc = readLine()!.split(separator: " ").map { Int($0)! }
 precondition(cc.allSatisfy { 1 <= $0 && Double($0) <= pow(10, 9) })
 
-var stack: [Int] = Array(cc[0...(K - 1)])
+var stack = cc[0...(K - 1)]
 var answer = Set(stack).count
 if N != K {
     for i in K...(N - 1) {
-        stack.removeFirst()
+        _ = stack.popFirst()
         stack.append(cc[i])
         let count = Set(stack).count
         answer = max(answer, count)
