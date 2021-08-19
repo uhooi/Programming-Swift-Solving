7ecdf650b335305a900ac05d8641ff995def0df1
uhooi <the_uhooi@yahoo.co.jp>
2021-08-19 22:44:45 +0900

    Add algo-method 304

---
+++ AlgoMethod/304.swift
@@ -0,0 +1,11 @@
+// 動的計画法 1-3
+// https://algo-method.com/tasks/304
+
+let N = Int(readLine()!)!
+precondition(1 <= N && N <= 40)
+
+var dp: [Int] = [1, 1]
+for i in 2...N {
+    dp.append(dp[i - 2] + dp[i - 1])
+}
+print(dp.last!)
