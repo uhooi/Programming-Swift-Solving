93b25910df54088d49b22badf6fb84417efd5053
uhooi <the_uhooi@yahoo.co.jp>
2021-08-19 09:06:55 +0900

    Add algo-method 303

---
+++ AlgoMethod/303.swift
@@ -0,0 +1,15 @@
+// 動的計画法 1-2
+// https://algo-method.com/tasks/303
+
+let N = Int(readLine()!)!
+precondition(2 <= N && N <= 100)
+
+let aa = readLine()!.split(separator: " ").map { Int($0)! }
+precondition(aa.count == N)
+precondition(aa.allSatisfy { 1 <= $0 && $0 <= 100 })
+
+var dp: [Int] = [0, aa[1]]
+for i in 2...(N - 1) {
+    dp.append(min(dp[i - 1] + aa[i], dp[i - 2] + aa[i] * 2))
+}
+print(dp.last!)
