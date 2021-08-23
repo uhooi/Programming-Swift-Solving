86faebb0f8e3c041dc8e6164ec49749c6a585f71
uhooi <the_uhooi@yahoo.co.jp>
2021-08-23 22:58:29 +0900

    Fix algo-method 323

--- AlgoMethod/323.swift
+++ AlgoMethod/323.swift
@@ -10,21 +10,12 @@ let dd = readLine()!.split(separator: " ").map { Int($0)! }
 precondition(dd.count == M)
 precondition(dd.allSatisfy { 1 <= $0 && $0 <= 1_000 })
 
-let removedDD = dd.filter { $0 <= N }
-
-var answer = false
-var dp: Set<Int> = Set(removedDD)
-while !dp.allSatisfy({ $0 > N }) {
-    if dp.contains(N) {
-        answer = true
+var dp: [Bool] = .init(repeating: false, count: N + 1)
+dp[0] = true
+for square in 1...N {
+    for dice in dd where square - dice >= 0 && dp[square - dice] {
+        dp[square] = true
         break
     }
-    var tmpDp: Set<Int> = []
-    for i in dp {
-        for j in removedDD {
-            tmpDp.insert(i + j)
-        }
-    }
-    dp = tmpDp
 }
-print(answer ? "Yes" : "No")
+print(dp[N] ? "Yes" : "No")
