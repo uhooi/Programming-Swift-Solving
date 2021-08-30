f4d83b1300cac5a5fa0e8f94bc435a25905efd18
uhooi <the_uhooi@yahoo.co.jp>
2021-08-31 00:16:41 +0900

    Refactor algo-method 336

--- AlgoMethod/336.swift
+++ AlgoMethod/336.swift
@@ -1,25 +1,37 @@
 // 動的計画法 3-1
 // https://algo-method.com/tasks/336
 
-let NM = readLine()!.split(separator: " ").map { Int($0)! }
-let (N, M) = (NM[0], NM[1])
+// MARK: Functions
+
+private func readInt2() -> (Int, Int) {
+    let values = readLine()!.split(separator: " ").map { Int(String($0))! }
+    precondition(values.count == 2)
+    return (values[0], values[1])
+}
+
+private func readIntArray() -> [Int] {
+    readLine()!.split(separator: " ").map { Int(String($0))! }
+}
+
+// MARK: Main
+
+let (N, M) = readInt2()
 precondition(2 <= N && N <= 100)
 precondition(1 <= M && M <= 10_000)
 
-let aa: [Int] = readLine()!.split(separator: " ").map { Int($0)! }
+let aa = readIntArray()
 precondition(aa.count == N - 1)
 precondition(aa.allSatisfy { 1 <= $0 && $0 <= 1_000 })
 
-var dp: [Bool] = .init(repeating: false, count: M)
-dp[0] = true
+var dp: [[Bool]] = .init(repeating: .init(repeating: false, count: M), count: N)
+dp[0][0] = true
 for row in 1..<N {
-    var tmpDp: [Bool] = dp
+    dp[row] = dp[row - 1]
     for column in 0..<M {
-        let destination = column + aa[row - 1]
-        if dp[column] && destination < M {
-            tmpDp[destination] = true
+        let dstColumn = column + aa[row - 1]
+        if dp[row - 1][column] && dstColumn < M {
+            dp[row][dstColumn] = true
         }
     }
-    dp = tmpDp
 }
-print(dp.filter { $0 } .count)
+print(dp[N - 1].lazy.filter { $0 } .count)
