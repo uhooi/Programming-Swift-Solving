d3cf4676a9c40c08d24d8fb2dc4e33af31dd6c53
uhooi <the_uhooi@yahoo.co.jp>
2021-09-05 23:44:07 +0900

    Fix algo-method 353

--- AlgoMethod/353.swift
+++ AlgoMethod/353.swift
@@ -1,8 +1,6 @@
 // Q3-7. ボールと 2 つの箱
 // https://algo-method.com/tasks/353
 
-// FIXME: TLE & MLE
-
 // MARK: Functions
 
 private func readInt() -> Int {
@@ -22,13 +20,12 @@ let ww = readIntArray()
 precondition(ww.count == N)
 precondition(ww.allSatisfy { 1 <= $0 && $0 <= 1_000 })
 
-var dp: [[Int]] = .init(repeating: [], count: N + 1)
-dp[0].append(ww[0])
-dp[0].append(-ww[0])
-for row in 1..<(N + 1) {
+var dp: [Set<Int>] = .init(repeating: [], count: N + 1)
+dp[1].insert(ww[0])
+for row in 2..<(N + 1) {
     for sum in dp[row - 1] {
-        dp[row].append(sum + ww[row - 1])
-        dp[row].append(sum - ww[row - 1])
+        dp[row].insert(abs(sum + ww[row - 1]))
+        dp[row].insert(abs(sum - ww[row - 1]))
     }
 }
-print(dp[N].lazy.map { abs($0) } .min()!)
+print(dp[N].min()!)
