b2142b14a6c69f4dc8c0d19747db7c7ff169f08e
uhooi <the_uhooi@yahoo.co.jp>
2021-09-14 10:24:04 +0900

    Add algo-method 368

---
+++ AlgoMethod/368_binary_search.swift
@@ -0,0 +1,26 @@
+// Q2-1. 方程式を解く
+// https://algo-method.com/tasks/368
+
+// MARK: Functions
+
+private func readInt() -> Int {
+    Int(readLine()!)!
+}
+
+// MARK: Main
+
+let N = readInt()
+precondition(3 <= N && N <= 100_000)
+
+var answer: Double = 0.0
+var left: Double = 0.0
+var right = min(Double(N), 100.0)
+while right - left > 0.01 {
+    let x = (left + right) / 2.0
+    if x * (x * (x + 1) + 2) + 3 >= Double(N) { // ???: 等号を含めていいかわかっていない
+        right = x
+    } else {
+        left = x
+    }
+}
+print(left) // ???: `left` を出力するのでいいかわかっていない
