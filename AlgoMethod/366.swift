d8429ca2b29d7334ac2afe4367ca706d3dca238b
uhooi <the_uhooi@yahoo.co.jp>
2021-09-25 00:36:17 +0900

    Refactor algo-method 366

--- AlgoMethod/366.swift
+++ AlgoMethod/366.swift
@@ -1,6 +1,8 @@
 // Q2-7. 貯金 (2)
 // https://algo-method.com/tasks/366
 
+// FIXME: WA
+
 import Foundation
 
 // MARK: Functions
@@ -26,18 +28,10 @@ precondition(xx.allSatisfy { 1 <= $0 && Double($0) <= pow(10, 18)})
 
 for k in 0...(N - 1) {
     var left = -1
-    var right = 100_000_000_000_000_000
+    var right = 1_000_000_000
     while (right - left) > 1 {
         let mid = (left + right) / 2
-        let sum: Decimal
-        if mid % 2 == 0 {
-            sum = Decimal(mid + 1) * Decimal(mid / 2)
-        } else {
-            let a = Decimal(mid + 1) * Decimal((mid - 1) / 2)
-            let b = Decimal((mid + 1) / 2)
-            sum = a + b
-        }
-        if sum >= Decimal(xx[k]) {
+        if (((mid + 1) * mid) / 2) >= xx[k] {
             right = mid
         } else {
             left = mid
