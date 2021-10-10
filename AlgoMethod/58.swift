4676d73c37aab61f9bc0d9d03f19269512ea353a
uhooi <the_uhooi@yahoo.co.jp>
2021-10-10 16:45:14 +0900

    Add algo-method 58

---
+++ AlgoMethod/58.swift
@@ -0,0 +1,26 @@
+// 標準入力3-9
+// https://algo-method.com/tasks/58
+
+// MARK: Functions
+
+private func readInt() -> Int {
+    Int(readLine()!)!
+}
+
+private func readNewLineStringArray(_ count: Int) -> [String] {
+    precondition(count >= 1)
+    return (1...count).map { _ in readLine()! }
+}
+
+// MARK: Inputs
+
+let N = readInt()
+precondition(1 <= N && N <= 1_000)
+
+let ss = readNewLineStringArray(N)
+precondition(ss.count == N)
+precondition(ss.allSatisfy { 1 <= $0.count && $0.count <= 2_000 })
+
+// MARK: Main
+
+print(ss.reduce("") { $0 + String(Array($1)[0])})
