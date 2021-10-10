2d7b4b4522484914a67bba67f3d5910ed86d34a7
uhooi <the_uhooi@yahoo.co.jp>
2021-10-10 13:24:42 +0900

    Refactor algo-method 30

--- AlgoMethod/30.swift
+++ AlgoMethod/30.swift
@@ -9,25 +9,13 @@ private func readInt2() -> (Int, Int) {
     return (values[0], values[1])
 }
 
-// MARK: Extensions
-
-private extension Numeric where Self: LosslessStringConvertible {
-    var digits: [Int] { string.digits }
-}
-private extension LosslessStringConvertible {
-    var string: String { String(self) }
-}
-private extension StringProtocol {
-    var digits: [Int] { compactMap { $0.wholeNumberValue } }
-}
-
 // MARK: Inputs
 
 let (A, B) = readInt2()
 precondition(1 <= A && A <= 100)
 precondition(1 <= B && B <= 100)
-precondition(A.digits.last! != B.digits.last!)
+precondition(A % 10 != B % 10)
 
 // MARK: Main
 
-print(A.digits.last! < B.digits.last! ? A : B)
+print(A % 10 < B % 10 ? A : B)
