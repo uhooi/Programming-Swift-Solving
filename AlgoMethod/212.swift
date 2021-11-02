838c6a49d546c8eba59745ee917f8803ceed14b1
uhooi <the_uhooi@yahoo.co.jp>
2021-11-02 22:50:52 +0900

    Add algo-method 212

---
+++ AlgoMethod/212.swift
@@ -0,0 +1,26 @@
+// 配列の全探索 3
+// https://algo-method.com/tasks/212
+
+// MARK: Functions
+
+private func readInt() -> Int {
+    Int(readLine()!)!
+}
+
+private func readIntArray() -> [Int] {
+    readLine()!.split(separator: " ").map { Int(String($0))! }
+}
+
+// MARK: Inputs
+
+let N = readInt()
+precondition(1 <= N && N <= 100)
+
+let aa = readIntArray()
+precondition(aa.count == N)
+precondition(aa.indices.allSatisfy { -100 <= $0 && $0 <= 100 })
+
+// MARK: Main
+
+let answer = aa.filter { $0 > 0 } .count
+print(answer)
