01cb9b8cd88db28872b65852949aa4dcae8b111d
uhooi <the_uhooi@yahoo.co.jp>
2021-11-02 23:14:50 +0900

    Add algo-method 216

---
+++ AlgoMethod/216.swift
@@ -0,0 +1,30 @@
+// 配列の全探索 4
+// https://algo-method.com/tasks/216
+
+// MARK: Functions
+
+private func readInt2() -> (Int, Int) {
+    let values = readLine()!.split(separator: " ").map { Int(String($0))! }
+    precondition(values.count == 2)
+    return (values[0], values[1])
+}
+
+private func readIntArray() -> [Int] {
+    readLine()!.split(separator: " ").map { Int(String($0))! }
+}
+
+// MARK: Inputs
+
+let (N, V) = readInt2()
+precondition(1 <= N && N <= 100)
+precondition(1 <= V && V <= 100)
+
+let aa = readIntArray()
+precondition(aa.count == N)
+precondition(aa.indices.allSatisfy { 1 <= $0 && $0 <= 100 })
+
+// MARK: Main
+
+let index = aa.lastIndex(of: V)
+let answer = index != nil ? index! : -1
+print(answer)
