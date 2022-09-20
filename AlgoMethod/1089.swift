218be879cde06889b3c90dce4d177090483c7609
uhooi <the_uhooi@yahoo.co.jp>
2022-09-20 22:50:22 +0900

    Add algo-method 1089

---
+++ AlgoMethod/1089.swift
@@ -0,0 +1,29 @@
+// Q. 4 と 6 の Fizz Buzz
+// https://algo-method.com/tasks/1089
+
+// MARK: Functions
+
+private func readInt() -> Int {
+    Int(readLine()!)!
+}
+
+// MARK: Inputs
+
+let N = readInt()
+precondition(1 <= N && N <= 100)
+
+// MARK: Main
+
+for n in 1...N {
+    var answer = ""
+    if n % 4 == 0 {
+        answer = "Fizz"
+    }
+    if n % 6 == 0 {
+        answer += "Buzz"
+    }
+    if n % 4 != 0 && n % 6 != 0 {
+        answer = "\(n)"
+    }
+    print(answer)
+}
