4c6384d904c7c3d3f0006423f5e8a4c2c459163d
uhooi <the_uhooi@yahoo.co.jp>
2022-09-22 22:48:28 +0900

    Add 1090.swift

---
+++ AlgoMethod/1090.swift
@@ -0,0 +1,23 @@
+// Q. 市松模様
+// https://algo-method.com/tasks/1090
+
+// MARK: Functions
+
+private func readInt4() -> (Int, Int, Int, Int) {
+    let values = readLine()!.split(separator: " ").map { Int(String($0))! }
+    precondition(values.count == 4)
+    return (values[0], values[1], values[2], values[3])
+}
+
+// MARK: Inputs
+
+let (H, W, p, q) = readInt4()
+precondition(1 <= H && H <= 1_000)
+precondition(1 <= W && W <= 1_000)
+precondition(0 <= p && p <= H - 1)
+precondition(0 <= q && q <= W - 1)
+
+// MARK: Main
+
+print((p + q + 2) % 2 == 0 ? "Black" : "White")
+
