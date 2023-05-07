1ec811c9dca7b620401ca6646c449377b120e888
uhooi <the_uhooi@yahoo.co.jp>
2023-05-07 22:58:01 +0900

    Add 1318

---
+++ AlgoMethod/1318.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+let radius: Double = 5
+let pi = 3.14
+
+print(2 * pi * radius)
+print(pi * pow(radius, 2))
+print(pi * pow(radius, 2) * 10)
+print(4 * pi * pow(radius, 2))
+print(4 / 3 * pi * pow(radius, 3))
---
+++ AlgoMethod/1318_question.swift
@@ -0,0 +1,8 @@
+// 変数をまとめる
+// https://algo-method.com/tasks/1318
+
+print(2 * 5 * 3.14)
+print(5 * 5 * 3.14)
+print(5 * 5 * 3.14 * 10)
+print(4 * 5 * 5 * 3.14)
+print(4 / 3.0 * 5 * 5 * 5 * 3.14)
