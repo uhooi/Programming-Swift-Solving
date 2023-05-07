9b515adf631cae180869805a6c5a6c08f66c4825
uhooi <the_uhooi@yahoo.co.jp>
2023-05-07 22:40:11 +0900

    Add 1316

---
+++ AlgoMethod/1316.swift
@@ -0,0 +1,2 @@
+let answer = (1...10).reduce(1, *)
+print(answer)
---
+++ AlgoMethod/1316_question.swift
@@ -0,0 +1,32 @@
+// for文でまとめる
+// https://algo-method.com/tasks/1316
+
+// int: answer = 1
+// 
+// answer = answer * 1
+// answer = answer * 2
+// answer = answer * 3
+// answer = answer * 4
+// answer = answer * 5
+// answer = answer * 6
+// answer = answer * 7
+// answer = answer * 8
+// answer = answer * 9
+// answer = answer * 10
+// 
+// print(answer)
+
+var answer = 1
+
+answer = answer * 1
+answer = answer * 2
+answer = answer * 3
+answer = answer * 4
+answer = answer * 5
+answer = answer * 6
+answer = answer * 7
+answer = answer * 8
+answer = answer * 9
+answer = answer * 10
+
+print(answer)
