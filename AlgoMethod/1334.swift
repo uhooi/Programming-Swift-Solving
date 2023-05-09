e6d8bf513398150de09a1d88341d37725361efcb
uhooi <the_uhooi@yahoo.co.jp>
2023-05-09 21:41:03 +0900

    Refactor 1334

--- AlgoMethod/1334.swift
+++ AlgoMethod/1334.swift
@@ -1,18 +1,11 @@
-func calcPoint(day: Int, amount: Int) -> Int {
-    var point = amount / 100
+func point(day: Int, amount: Int) -> Int {
+    let basePoint = amount / 100
     let isSpecialDay = (day % 10 == 5)
-    if isSpecialDay {
-        point *= 3
-    }
-    return point
+    return basePoint * (isSpecialDay ? 3 : 1)
 }
 
-let purchaseDayList = [4, 5, 8, 18, 25]
-let purchaseAmountList = [700, 1200, 3000, 600, 2800]
-var pointTotal = 0
-for index in 0..<purchaseDayList.count {
-    let day = purchaseDayList[index]
-    let amount = purchaseAmountList[index]
-    pointTotal += calcPoint(day: day, amount: amount)
-}
+let purchaseDays = [4, 5, 8, 18, 25]
+let purchaseAmounts = [700, 1200, 3000, 600, 2800]
+let pointTotal = (0..<purchaseDays.count)
+    .reduce(0) { $0 + point(day: purchaseDays[$1], amount: purchaseAmounts[$1]) }
 print(pointTotal)
