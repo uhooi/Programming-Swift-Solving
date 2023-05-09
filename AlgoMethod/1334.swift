cd8bfde395dbf36cf44fc2b135269324bedf3f5f
uhooi <the_uhooi@yahoo.co.jp>
2023-05-09 21:47:56 +0900

    Refactor 1334

--- AlgoMethod/1334.swift
+++ AlgoMethod/1334.swift
@@ -7,5 +7,6 @@ func point(day: Int, amount: Int) -> Int {
 let purchaseDays = [4, 5, 8, 18, 25]
 let purchaseAmounts = [700, 1200, 3000, 600, 2800]
 let pointTotal = (0..<purchaseDays.count)
-    .reduce(0) { $0 + point(day: purchaseDays[$1], amount: purchaseAmounts[$1]) }
+    .map { point(day: purchaseDays[$0], amount: purchaseAmounts[$0]) }
+    .reduce(0, +)
 print(pointTotal)
